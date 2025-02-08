04466a3590a5c11a11bea2d075487bd387806daa
Anuj Gupta <anjkk01@gmail.com>
2025-02-08 15:23:53 +0530

    doctorDashBoard added

--- doctorDashBoard.js
+++ doctorDashBoard.js
@@ -10,12 +10,9 @@ const doctorDashboard = async (req, res) => {
 
         const appointments = await prisma.doctor_appointment.findMany({
             where: { doctor_id },
-            orderBy: [
-                { appointment_date: "desc" },
-                { time_from: "desc" }
-            ],
+            
         });
-        
+
         return res.status(200).json({ appointments });
     } catch (error) {
         console.error("Error fetching doctor appointments:", error);
